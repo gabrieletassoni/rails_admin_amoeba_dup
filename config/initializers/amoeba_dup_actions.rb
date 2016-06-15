b79cdc26ad849fb344d2b8b166e04a7408a5c219
Gabriele Tassoni <gabrieletassoni@taris.it>
2016-06-15 11:53:43 +0200

    Another little step in complete encapsulation way

--- config/initializers/amoeba_dup_actions.rb
+++ config/initializers/amoeba_dup_actions.rb
@@ -7,8 +7,14 @@
 #   Rails.logger.debug "TheCoreActions not exists, cannot module_eval it please add the action to rails_admin.rb initializer."
 # end
 
-TheCoreActions.module_eval do
-  def amoeba_dup_actions
+RailsAdmin.config do |config|
+  config.actions do
     amoeba_dup
   end
 end
+
+# TheCoreActions.module_eval do
+#   def amoeba_dup_actions
+#     amoeba_dup
+#   end
+# end
--- lib/rails_admin_amoeba_dup/engine.rb
+++ lib/rails_admin_amoeba_dup/engine.rb
@@ -1,3 +1,6 @@
+
+require 'amoeba'
+
 module RailsAdminAmoebaDup
   class Engine < ::Rails::Engine
   end
